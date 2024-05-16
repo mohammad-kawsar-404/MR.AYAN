75893e71febe42935d99bb381da516b4dc8a66e4
mohammad-kawsar-404 <mi1801773@gmail.com>
2024-05-16 22:28:17 +0600

    Update sad.js

--- modules/commands/sad.js
+++ modules/commands/sad.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["🙂SAD-VIDEO🙂\n \nOwner: AYAN CHOWDHURY🥀"];
+   var hi = ["🙂SAD-VIDEO🙂\n \nOwner: MOHAMMAD KAWSAR🥀"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
