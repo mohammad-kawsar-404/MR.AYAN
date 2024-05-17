23d2dd2bd10bb679fa95f8c1dc81e0781bd5bb3a
mohammad-kawsar-404 <mi1801773@gmail.com>
2024-05-17 07:32:57 +0600

    Update 1 info.js

--- modules/commands/1 info.js
+++ modules/commands/1 info.js
@@ -25,7 +25,7 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =[
-"https://i.postimg.cc/LXp4CN1T/received-325524310216120.jpg",
+"https://i.imgur.com/e3ozMwK.jpeg",
 ];
 var callback = () => api.sendMessage({body:` ♛|| Admin and Bot Info ||♛
 
